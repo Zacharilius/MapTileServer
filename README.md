d6c98b44460ba060482b6137e07f23822f6225f4
Zacharilius <zabensley@gmail.com>
2017-10-29 08:47:13 -0700

    Updated README instructions. Styled map.

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 ## Development
 
-Run Development Server
+Run Development Servers in 2 seperate terminal tabs.
 `npm run develop`
+`npm run devWebpack`
--- src/main.js
+++ src/main.js
@@ -12,27 +12,27 @@ const zoom = 1;
 const map = (
   <Map center={position} zoom={zoom}>
         <LayersControl position="topright">
-          <BaseLayer checked name="OpenStreetMap.Mapnik">
+          <Overlay checked name="OpenStreetMap.Mapnik">
             <TileLayer
               attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-          </BaseLayer>
-          <BaseLayer name="Country Boundaries">
+          </Overlay>
+          <Overlay name="Country Boundaries">
             <TileLayer
               url="/api/tiles/ne_110m_countries/{z}/{x}/{y}.png"
             />
-          </BaseLayer>
-          <BaseLayer name="USA State Boundaries">
+          </Overlay>
+          <Overlay name="USA State Boundaries">
             <TileLayer
               url="/api/tiles/ne_110m_usa_state_boundaries/{z}/{x}/{y}.png"
             />
-          </BaseLayer>
-          <BaseLayer name="Seattle Neighborhoods">
+          </Overlay>
+          <Overlay name="Seattle Neighborhoods">
             <TileLayer
               url="/api/tiles/seattle_neighborhoods/{z}/{x}/{y}.png"
             />
-          </BaseLayer>
+          </Overlay>
         </LayersControl>
   </Map>
 );
