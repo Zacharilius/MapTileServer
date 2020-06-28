2abbab0f7a707d5609dde88d6527700972f0ace8
Zachary Bensley <zacharyb@qualtrics.com>
2020-06-28 15:32:51 -0700

    Refactor mapnik tile creation/getter to tile-service.

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,17 +2,15 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as express from 'express';
 import * as mapnik from 'mapnik';
-import * as mkdirp from 'mkdirp';
 
-import * as mercator from './sphericalmercator';
+import { getTile } from './tile-service';
 
 /* =============================================================================
 	Config data
 */
 
-const hostname = '127.0.0.1'
-	, port = 8080
-	, TILE_SIZE = 256;
+const hostname = '127.0.0.1';
+const port = 8080;
 
 const app = express();
 
@@ -33,115 +31,18 @@ app.get('/', (req, res) => {
 	}
 });
 
-// TODO: Implement
-app.get('/api/tiles', (req, res) => {
-	try {
-		res.writeHead(200, {'Content-Type': 'application/json'});
-		res.end(JSON.stringify({data: getTileInfos()}));
-	} catch (err) {
-		res.writeHead(500);
-		res.end('Error: ', err);
-	}
-});
+app.get('/api/tile/:tileId/:z/:x/:y.png', async (req, res) => {
+	const tileId = req.params.tileId;
+	const z = req.params.z
+	const x = req.params.x
+	const y = req.params.y;
 
-declare interface MapnikImage {
-    new(x: number, y: number): () => void;
-	encode(type: string, callback?: (err: Error, buffer: Buffer) => void): void;
-	encodeSync(type: string);
-    getData(): Buffer;
-    save(fp: string): () => void;
-    open(fp: string): () => void;
-}
-
-declare type BBox = [number, number, number, number]
-
-declare interface MapnikMap {
-    constructor(x: number, y: number)
-    load(xml: string, callback?: (err: Error, map: MapnikMap) => void): void;
-    render(images: MapnikImage, callback?: (err: Error, map: MapnikImage) => void): void;
-    bufferSize?: number
-    extent?: BBox;
-
-}
-
-app.get('/api/tile/:tileId/:z/:x/:y.png', (req, res) => {
-	let [tileId, z, x, y] = [req.params.tileId, req.params.z, req.params.x, req.params.y];
-	let tileArgs = [tileId, z, x, y];
-
-	if (doesTileExist(tileArgs)) {
-		// Use computed image
-		var image = getTileImage(tileArgs);
-		res.writeHead(200, {'Content-Type': 'image/png'});
-		res.end(image);
-	} else {
-		const map: MapnikMap = new mapnik.Map(TILE_SIZE, TILE_SIZE);
-		map.bufferSize = 64;
-		map.load(`./map-data/${tileId}.xml`, (err, map) => {
-			if (err) throw err;
-
-			let mapnikImage: MapnikImage = new mapnik.Image(TILE_SIZE, TILE_SIZE);
-			let bbox = mercator.xyz_to_envelope(+x,+y,+z, false);
-			map.extent = bbox;
-			map.render(mapnikImage, (err, image) => {
-				if (err) throw err;
-
-				saveTileImage(mapnikImage, tileArgs);
-
-				res.writeHead(200, {'Content-Type': 'image/png'});
-				res.end(image.encodeSync('png'));
-			});
-		});
-	}
+	const image = await getTile(tileId, z, x, y);
+	res.writeHead(200, {'Content-Type': 'image/png'});
+	res.end(image);
 });
 
 // Server init
 app.listen(port, () => {
 	console.log(`Server listening at: http://${hostname}:${port}/`);
 });
-
-/* =============================================================================
-	Database
-*/
-
-// TODO: Store tiles in database
-function getTileInfos() {
-	return {};
-}
-
-/* =============================================================================
-	Util
-*/
-
-function getTileDirPath(args) {
-	let [tileId, z, x, y] = args,
-		dir = `./tiles/${tileId}/${z}/${x}`;
-	return dir
-}
-
-function getTileImagePath(args) {
-	let [tileId, z, x, y] = args,
-		dir = getTileDirPath(args),
-		pngPath = `${dir}/${y}.png`;
-	return pngPath;
-}
-
-function doesTileExist(args) {
-	var pngPath = getTileImagePath(args);
-	return fs.existsSync(pngPath);
-}
-
-function getTileImage(args) {
-	var imagePath = getTileImagePath(args);
-	var img = fs.readFileSync(imagePath);
-	return img;
-}
-
-function saveTileImage(image, args) {
-	let dir = getTileDirPath(args),
-		pngPath = getTileImagePath(args);
-
-	if (!fs.existsSync(dir)) {
-		mkdirp.sync(dir);
-	}
-	image.save(pngPath, 'png32');
-}
---
+++ src/server/tile-service.ts
@@ -0,0 +1,84 @@
+import * as fs from 'fs';
+import * as mapnik from 'mapnik';
+import * as mkdirp from 'mkdirp';
+
+import * as mercator from './sphericalmercator';
+
+const TILE_SIZE: number = 256;
+
+declare interface MapnikImage {
+	new(x: number, y: number): () => void;
+	encode(type: string, callback?: (err: Error, buffer: Buffer) => void): void;
+	encodeSync(type: string);
+	getData(): Buffer;
+	save(fp: string): () => void;
+	open(fp: string): () => void;
+}
+
+declare type BBox = [number, number, number, number]
+
+declare interface MapnikMap {
+	constructor(x: number, y: number)
+	load(xml: string, callback?: (err: Error, map: MapnikMap) => void): void;
+	render(images: MapnikImage, callback?: (err: Error, map: MapnikImage) => void): void;
+	bufferSize?: number
+	extent?: BBox;
+
+}
+
+export const getTile = async (tileId: string, z: string, x: string, y: string): Promise<Buffer> => {
+	if (doesTileExist(tileId, z, x, y)) {
+		return getTileImage(tileId, z, x, y);
+	} else {
+		const map: MapnikMap = new mapnik.Map(TILE_SIZE, TILE_SIZE);
+		map.bufferSize = 64;
+		return new Promise((resolve, reject) => {
+			map.load(`./map-data/${tileId}.xml`, (err, map) => {
+				if (err) {
+					reject(err)
+				};
+	
+				let mapnikImage: MapnikImage = new mapnik.Image(TILE_SIZE, TILE_SIZE);
+				let bbox = mercator.xyz_to_envelope(+x,+y,+z, false);
+				map.extent = bbox;
+				map.render(mapnikImage, (err, image) => {
+					if (err) {
+						reject(err)
+					};
+	
+					saveTileImage(mapnikImage, tileId, z, x, y);
+					resolve(image.encodeSync('png'));
+				});
+			});
+		});
+	}
+}
+
+const getTileDirPath = (tileId: string, z: string, x: string): string => {
+	return `./tiles/${tileId}/${z}/${x}`;
+}
+
+const getTileImagePath = (tileId: string, z: string, x: string, y: string): string => {
+	const dir = getTileDirPath(tileId, z, x);
+	return `${dir}/${y}.png`;
+}
+
+const doesTileExist = (tileId: string, z: string, x: string, y: string): boolean => {
+	return fs.existsSync(getTileImagePath(tileId, z, x, y));
+}
+
+const getTileImage = (tileId: string, z: string, x: string, y: string): Buffer => {
+	var imagePath = getTileImagePath(tileId, z, x, y);
+	var img = fs.readFileSync(imagePath);
+	return img;
+}
+
+const saveTileImage = (image, tileId: string, z: string, x: string, y: string): void => {
+	let dir = getTileDirPath(tileId, z, x),
+	pngPath = getTileImagePath(tileId, z, x, y);
+
+	if (!fs.existsSync(dir)) {
+		mkdirp.sync(dir);
+	}
+	image.save(pngPath, 'png32');
+}
