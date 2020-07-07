c34292350d438cf3350810580b1160e81cd147b1
Zachary Bensley <zacharyb@qualtrics.com>
2020-07-06 19:36:19 -0700

    Add main.tsx that was not added with last PR.

---
+++ src/client/main.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import { render } from 'react-dom'
+
+import * as Leaflet from 'leaflet';
+import {
+    LayersControl,
+    Map,
+    TileLayer,
+} from 'react-leaflet'
+
+const { Overlay } = LayersControl
+const position: Leaflet.LatLngExpression = [40, -80]
+const zoom = 2
+
+const map = (
+    <Map center={position} zoom={zoom}>
+        <LayersControl position="topright">
+            <Overlay checked name="OpenStreetMap.Mapnik">
+                <TileLayer
+                    attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+            </Overlay>
+            <Overlay name="Country Boundaries">
+                <TileLayer
+                    url="/api/tile/ne_110m_countries/{z}/{x}/{y}.png"
+                />
+            </Overlay>
+            <Overlay name="USA State Boundaries">
+                <TileLayer
+                    url="/api/tile/ne_110m_usa_state_boundaries/{z}/{x}/{y}.png"
+                />
+            </Overlay>
+            <Overlay name="Seattle Neighborhoods">
+                <TileLayer
+                    url="/api/tile/seattle_neighborhoods/{z}/{x}/{y}.png"
+                />
+            </Overlay>
+        </LayersControl>
+    </Map>
+)
+
+render(map, document.getElementById('map'))
