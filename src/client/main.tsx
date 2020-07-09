8280e489b66778865b7c06c0e9c65aac10bd7203
Zachary Bensley <zacharyb@qualtrics.com>
2020-07-08 20:10:44 -0700

    Import CSS and HTML from react.

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react'
 import { render } from 'react-dom'
 
+import './index.html'
+
+import 'leaflet/dist/leaflet.css'
+import './Main.css'
+
 import Map from './components/Map'
 
 render(<Map />, document.getElementById('map'))
--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,33 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(html|png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              publicPath: 'static'
+            }
+          },
+        ],
+      },
+      {
+        test: /\.(html)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              publicPath: 'static',
+              name: '[name].[ext]',
+            }
+          },
+        ],
+      },
     ],
   },
   resolve: {
